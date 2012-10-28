d13adce62b17563f836eff9c70360e848ea3c264
kalibuda@hotmail.com <kalibuda@hotmail.com>
2012-10-28 14:36:20 +0000

    Started Divide Integers Calculator

---
+++ src/divideIntegers/Calculator.java
@@ -0,0 +1,21 @@
+package divideIntegers;
+
+public class Calculator {
+
+	/*
+	 * Create a Java class called Calculator. 
+	 * The class should implement the following methods, each of them printing
+	 * the result on the screen.
+	 * add(int, int)
+	 * subtract(int, int)
+	 * multiply(int, int)
+	 * divide(int, int)
+	 * modulus(int, int)
+	 */
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
