37904e1bbc7b5b57abc480f2083fb9da3e32efab
kalibuda@hotmail.com <kalibuda@hotmail.com>
2012-10-28 22:11:24 +0000

    Done

--- src/copyingArrays/ArrayCopier.java
+++ src/copyingArrays/ArrayCopier.java
@@ -1,11 +1,48 @@
 package copyingArrays;
 
-public class ArrayCopier {
 
-	public void copy(int[] src, int[] dst){
+public class ArrayCopier {
+	
+	//reference or pointer fields
+	private int[] src;
+	private int[] dst;
+	
+	
+	///////////////////////////////////////////////////////////
+	// Constructor
+	//////////////////////////////////////////////////////////
+	public ArrayCopier(int[] first, int[] second) {
+		
+		// where they are pointing to
+		src = first;
+		dst = second;
 		
 	}
+
+	///////////////////////////////////////////////////////////
+	// copy()
+	//////////////////////////////////////////////////////////
+	public void copy()
+	{
 	
+		int counter = 0;
+		for (int k = 0; k <= dst.length-1; k++){
+			
+			if(counter <= src.length-1){
+				dst[k] = src[k];
+			}else if (counter <= dst.length-1){
+				dst[k] = 0;
+			}
+			counter++;
+		}
+		
+		
+		System.out.println("Second Array");
+		for (int j = 0; j < dst.length; j++){
+			System.out.print(dst[j] + " | ");
+		}  
+		
+	} // \\\\\\\\\\\\\\\\\\\ //
 	
 	
 }
--- src/copyingArrays/CopyingArrays.java
+++ src/copyingArrays/CopyingArrays.java
@@ -1,5 +1,8 @@
 package copyingArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CopyingArrays {
 
 	/*
@@ -9,10 +12,13 @@ public class CopyingArrays {
 	 * should copy the elements of the first array 
 	 * (you can call it src, from ÒsourceÓ) to the second
 	 * one (dst, from ÒdestinationÓ) as much as possible.
+	 * 
 	 * If the second array is smaller, then only 
 	 * those elements that fit will be copied. 
 	 * If the second array is larger, it will be 
-	 * filled with zeroes. Write a program that creates 
+	 * filled with zeroes. 
+	 * 
+	 * Write a program that creates 
 	 * an object of this class and uses this method to 
 	 * copy some arrays in all three cases:
 	 * 
@@ -20,9 +26,47 @@ public class CopyingArrays {
 	 * >> The source array is longer.
 	 * >> The source array is shorter.
 	 */
+	
+	
+	
+	///////////////////////////////////////////////////////////
+	// main()
+	//////////////////////////////////////////////////////////
 	public static void main(String[] args) {
-		
-		
+	
+	// Scanner
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("Enter the size of the first array");
+	int firstArraySize = in.nextInt();
+	
+	System.out.print("Enter a list of Integers:");
+	
+	// first Array
+	int[] firstArray = new int[firstArraySize];
+	
+	// input first array
+	int currentSize = 0;
+	for (double i : firstArray){ 
+		firstArray[currentSize] = in.nextInt();
+		currentSize++;
 	}
+	
+	System.out.print("Enter the size of the second list: \n >>");
+	
+	// reset scanner
+	in.reset();
+	// size of second array
+	int[] second = new int[in.nextInt()];
+	
+	// create a array object
+	ArrayCopier olive = new ArrayCopier(firstArray, second);
+	// call the method 
+	olive.copy();
+	
+	
+		
+	
+	}// \\\\\\\\\\\\\\\\\\\ //
 
 }
